4bfdfdb9064d39d6e0d6d2ac72fd9a76e0cea92b
Thomas Siskos <thomas.siskos91@gmail.com>
2018-11-13 13:51:44 +0100

    Add objective function for hyperopt-parameter tuning

--- models/crime_total_lstm.py
+++ models/crime_total_lstm.py
@@ -10,46 +10,58 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
 
+from hyperopt import tpe, hp, fmin, STATUS_OK, Trials
+from hyperopt.pyll.base import scope
+
+
 datapath = os.path.join("data", "crime_total.csv")
 dataset = pd.read_csv(datapath, index_col="date",
                       dtype={"crimes_total": np.float32})
 
 
 class LSTMNET(Sequential):
-    def __init__(self, data, maxlag, lstm_neurons, input_shape=(1, 1),
-                 lstm_cells=1, lossfunc='mean_squared_error', optimizer='adam',
+    def __init__(self, data, maxlag, lstm_neurons, lstm_cells=1,
+                 lossfunc='mean_squared_error', optimizer='adam',
                  epochs=100, batch_size=1, train_size=0.8):
 
-        assert len(data.shape) == 1
+        assert len(data.shape) == 1 or data.shape[1] == 1
         assert maxlag >= 1
         assert 0 < train_size < 1
 
-        self.data = data
+        self.data = copy.deepcopy(data)
         self.maxlag = maxlag
-        self.old_shape = data.shape
-        self.new_shape = input_shape
         self.train_size = int(train_size * len(self.data))
+        self.batch_size = batch_size
+        self.lstm_neurons = lstm_neurons
+        self.lstm_cells = lstm_cells
+        self.lossfunc = lossfunc
+        self.optimizer = optimizer
+        self.epochs = epochs
 
         self.X_train, self.X_test, self.y_train, self.y_test = self.__stage()
+
+    def train(self):
         self.X_train, self.X_test = self.__transform_shape()
 
         # run parent-class' constructor
         super().__init__()
         # add LSTM cells
-        for _ in range(lstm_cells):
-            self.add(LSTM(lstm_neurons, input_shape=input_shape))
+        for _ in range(self.lstm_cells):
+            self.add(LSTM(self.lstm_neurons, input_shape=(1, self.maxlag)))
         # add final layer
         self.add(Dense(1))
-        self.compile(loss=lossfunc,
-                     optimizer=optimizer)
+        self.compile(loss=self.lossfunc,
+                     optimizer=self.optimizer)
         self.fit(self.X_train, self.y_train,
-                 epochs=epochs, batch_size=batch_size)
+                 epochs=self.epochs, batch_size=self.batch_size)
+
+        # self.X_train, self.X_test = self.__inverse_transform_shape()
 
     def __stage(self):
         """Prepare data for training."""
 
         target = self.data.columns
-        cols = ["lag_" + str(i) for i in range(1, maxlag+1)]
+        cols = ["lag_" + str(i) for i in range(1, self.maxlag+1)]
 
         # Create lagged variables
         for i, colname in enumerate(cols, 1):
@@ -69,28 +81,52 @@ class LSTMNET(Sequential):
         y_train, y_test = y[:self.train_size], y[self.train_size:]
         X_train, X_test = X[:self.train_size], X[self.train_size:]
 
+        self.old_train_shape = X_train.shape
+        self.old_test_shape = X_test.shape
+
         return X_train, X_test, y_train, y_test
 
     def __transform_shape(self):
         """Transform data into a feedable form for LSTM-layer."""
-        X_train = self.X_train.reshape(self.new_shape)
-        X_test = self.X_test.reshape(self.new_shape)
+        # Infer new shape
+        self.new_train_shape = (self.old_train_shape[0],
+                                1,
+                                self.maxlag)
+        self.new_test_shape = (self.old_test_shape[0],
+                               1,
+                               self.maxlag)
+
+        X_train = self.X_train.reshape(self.new_train_shape)
+        X_test = self.X_test.reshape(self.new_test_shape)
 
         return X_train, X_test
 
     def __inverse_transform_shape(self):
         """Transform data into tabular shape."""
-        X_train = self.X_train.reshape(self.old_shape)
-        X_test = self.X_test.reshape(self.old_shape)
+        X_train = self.X_train.reshape(self.old_train_shape)
+        X_test = self.X_test.reshape(self.old_test_shape)
 
         return X_train, X_test
 
+    # TODO: finish visualization of fit.
+    def plot_fit(self):
+        """Visualize fit."""
+        pass
 
-model = LSTMNET(data, maxlag=3, lstm_neurons=4, input_shape=(1, 3))
+
+model = LSTMNET(dataset, maxlag=3, lstm_neurons=4, lstm_cells=1,
+                epochs=10, batch_size=3)
+model.train()
 
 
 def objective(params):
-    model = LSTMNET(X_train, y_train, **params)
+    model = LSTMNET(dataset, **params)
+    model.train()
+    predictions = model.predict(model.X_test)
+    loss = mean_squared_error(y_true=model.y_test, y_pred=predictions)
+    return {'loss': loss, 'status': STATUS_OK}
 
 
-def prepare_data()
+paramspace = {
+
+}
