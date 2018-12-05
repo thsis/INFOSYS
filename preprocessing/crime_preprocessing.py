5b8bb76d32b9cd51a740a59634a3fdc1a6ada16c
Thomas Siskos <thomas.siskos91@gmail.com>
2018-12-05 08:23:39 +0100

    Add executable preprocessing script.

--- preprocessing/crime_preprocessing.py
+++ preprocessing/crime_preprocessing.py
@@ -1,16 +1,31 @@
 import os
+import numpy as np
 import pandas as pd
 
-"""
-Data: https://catalog.data.gov/dataset/crimes-2001-to-present-398a4
-"""
 
-datapath = os.path.join("data", "crimes.zip")
-data = pd.read_csv(datapath, header=0, parse_dates=["Date"], index_col="Date")
+datapath = os.path.join("..", "data", "crimes.zip")
+columns = ["ID", "Date", "Block", "Primary Type", "Latitude", "Longitude", "District", "Ward", "Community Area"]
+fulldata = pd.read_csv(datapath, usecols=columns)
 
-# Count all reported crime as the same.
-X = data.groupby(data.index.date).ID.count()
+fulldata.info()
+fulldata.head(10)
 
-# Write to csv.
-X.reset_index().to_csv(os.path.join("data", "crime_total.csv"),
-                       header=["date", "crimes_total"], index=False)
+fulldata["Date"] = pd.to_datetime(fulldata["Date"],
+                                  format='%m/%d/%Y',
+                                  exact=False)
+fulldata = fulldata.set_index("Date")
+
+print("Save `crimes_total.csv`")
+crimes_total = fulldata.groupby(fulldata.index.date).ID.count().reset_index()
+crimes_total.to_csv(os.path.join("..", "data", "crime_total.csv"),
+                    header=["date", "crimes_total"], index=False)
+
+
+crimes_district = fulldata.dropna(subset=["District"])
+crimes_district = crimes_district.groupby(["District", crimes_district.index]).ID.count()
+
+new_index = pd.MultiIndex.from_product(crimes_district.index.levels)
+crimes_district = crimes_district.reindex(new_index).fillna(0)
+print("Save `crimes_district.csv`.")
+crimes_district.reset_index().to_csv(os.path.join("..", "data", "crimes_district.csv"),
+                                     header=["District", "Date", "Incidents"], index=False)
